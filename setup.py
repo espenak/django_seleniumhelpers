5746960c7550303bde942759df2f7972c996bcf5
Espen A. Kristiansen <post@espenak.net>
2012-04-21 11:16:04 +0200

    setup: long_description and distribute.

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
-from os.path import join, dirname
 from setuptools import setup, find_packages
-
-this_dir = dirname(__file__)
-
-try:
-    f = open(join(this_dir, 'README.rst'))
-    long_description = f.read().strip()
-    f.close()
-except IOError:
-    long_description = None
 
 setup(name = 'django_seleniumhelpers',
       description = 'Extends django.test.LiveServerTestCase to make selenium testing even easier.',
@@ -17,9 +7,10 @@ setup(name = 'django_seleniumhelpers',
       url = 'https://github.com/espenak/django_seleniumhelpers',
       author = 'Espen Angell Kristiansen',
       packages=find_packages(exclude=['ez_setup']),
-      install_requires = ['setuptools', 'Django', 'selenium'],
+      install_requires = ['distribute', 'Django', 'selenium'],
       include_package_data=True,
       zip_safe=False,
+      long_description=open('README.rst').read(),
       classifiers=[
                    'Development Status :: 5 - Production/Stable',
                    'Environment :: Web Environment',
